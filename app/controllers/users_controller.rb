15345adc6453eff9a375f6ce36986cc1af40b78b
rloterh <rloterh@gmail.com>
2020-10-30 03:45:24 +0000

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @User =User.new
   end
 
   def create
@@ -9,5 +10,24 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
